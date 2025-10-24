c1b40ed9fab07c83517cc87717bac4fa01546d9a
赵熙龙 <1670470731@qq.com>
2025-10-24 11:16:24 +0800

    标准化了接口并增加了Swagger注释

--- src/main/java/com/zxl/xlforum/account/api/AccountApi.java
+++ src/main/java/com/zxl/xlforum/account/api/AccountApi.java
@@ -4,12 +4,14 @@ import com.zxl.xlforum.account.dto.req.AccountBaseRequest;
 import com.zxl.xlforum.account.dto.req.AccountLoginRequest;
 import com.zxl.xlforum.account.dto.req.AccountSignupRequest;
 import com.zxl.xlforum.account.dto.resp.AccountBaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,14 +25,18 @@ public interface AccountApi {
      * @param accountLoginRequest
      * @return JWTtoken以及用户信息
      */
-    public ResponseEntity<AccountBaseResponse> login(@Valid @RequestBody AccountLoginRequest accountLoginRequest);
+    @Operation(description = "登录")
+    @PostMapping("/login")
+    ResponseEntity<AccountBaseResponse> login(@Valid @RequestBody AccountLoginRequest accountLoginRequest);
 
     /**
      * 注册，需要传入邮箱，密码和用户名
      * @param accountSignupRequest
      * @return 注册状态
      */
-    public ResponseEntity<AccountBaseResponse> signup(@Valid @RequestBody AccountSignupRequest accountSignupRequest);
+    @Operation(description = "注册")
+    @PostMapping("signup")
+    ResponseEntity<AccountBaseResponse> signup(@Valid @RequestBody AccountSignupRequest accountSignupRequest);
 
     /**
      * 修改密码，在登陆后传入新密码和旧密码
@@ -38,7 +44,9 @@ public interface AccountApi {
      * @param newPassword
      * @return JWT新的token以及用户基本信息
      */
-    public ResponseEntity<AccountBaseResponse> changePassword(@Parameter(description = "用户密码")
+    @Operation(description = "修改密码")
+    @PostMapping("changePassword")
+    ResponseEntity<AccountBaseResponse> changePassword(@Parameter(description = "用户密码")
                                                               @Size(min = 6, max = 20, message = "密码应在6-20个字符内")
                                                               @RequestParam
                                                               String oldPassword,
@@ -52,5 +60,7 @@ public interface AccountApi {
      * @param accountLogoffRequest
      * @return
      */
-    public ResponseEntity<AccountBaseResponse> signoff(@Valid @RequestBody AccountLoginRequest accountLogoffRequest);
+    @Operation(description = "注销用户")
+    @PostMapping("signoff")
+    ResponseEntity<AccountBaseResponse> signoff(@Valid @RequestBody AccountLoginRequest accountLogoffRequest);
 }
